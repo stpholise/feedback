14fe50eb1e3285ed20113332b811875c347d2b06
stephen olise <stpholise@gmail.com>
2025-12-01 16:15:29 +0100

    update responsive view

--- app/feedback/_components/Feedbacks.tsx
+++ app/feedback/_components/Feedbacks.tsx
@@ -1,12 +1,12 @@
-
 import Pagination from "./Pagination";
+import { toast } from "react-toastify";
 
 interface FeedbackType {
   name: string;
   email: string;
   phone: string;
   message: string;
-  type?: string;
+  type: string;
 }
 
 const Feedbacks = async ({
@@ -26,77 +26,28 @@ const Feedbacks = async ({
     cache: "no-store",
   });
   if (!res.ok) {
-    console.log("error fetching product");
+    toast.error("error fetching product",
+      {
+        theme: "light",
+        hideProgressBar: true
+      }
+    );
   }
 
-  
-
   const data = (await res.json()) as FeedbackType[];
- 
-console.log("data")
+
   const filterValues = () => {
     return data
       .filter((item) => (type ? item.type == type : true))
       .filter((item) =>
-        search ? item.name.toLowerCase().includes(search.toLowerCase()) || item.email.toLowerCase().includes(search.toLowerCase()) || item.message.toLowerCase().includes(search.toLowerCase()): true
+        search
+          ? item.name.toLowerCase().includes(search.toLowerCase()) ||
+            item.email.toLowerCase().includes(search.toLowerCase()) ||
+            item.message.toLowerCase().includes(search.toLowerCase())
+          : true
       );
   };
- ;
-
-  return (
-    <div>
-      {<Pagination items={filterValues()} />}
-      {/* {filterdData.map((feedback, index) => (
-        <div key={index} className="border border-gray-200 rounded-lg px-4 py-4 text-[#555B64] bg-white text-sm">
-          <div className="flex items-center gap-2 mb-4  ">
-            <div className=" size-8 flex items-center justify-center  p-1  text-center rounded-full bg-gray-100 uppercase text-black  text-sm">
-              {feedback.name
-                .split(" ")
-                .map((word: string) => word[0])
-                .join("")}
-            </div>
-            <p className="text-black  font-medium text-base capitalize  h-fit  ">
-              {feedback.name}
-            </p>
-          </div>
-          <div className="flex items-start mt-2 w-full">
-            <Image
-              src={"/icons/email.svg"}
-              alt="email icon"
-              width={16}
-              height={16}
-              className="mr-2"
-            />
-            <p className="break-all">{feedback.email}</p>
-          </div>
-          <div className="flex items-center mt-2">
-            <Image
-              src={"/icons/phone.svg"}
-              alt="email icon"
-              width={16}
-              height={16}
-              className="mr-2"
-            />
-            <p className="text-black"></p>02230193845
-          </div>
-          <div className="flex items-start mt-2">
-            <Image
-              src={"/icons/bugs.svg"}
-              alt="email icon"
-              width={16}
-              height={16}
-              className="mr-2 "
-            />
-            <div className="">
-              <p className="text-black  font-mediom text-sm">Bugs</p>
-              <p className="">{feedback.message}</p>
-            </div>
-          </div>
-            <div className="">{feedback.type}</div>
-        </div>
-      ))} */}
-    </div>
-  );
+  return <div>{<Pagination items={filterValues()} />}</div>;
 };
 
 export default Feedbacks;
